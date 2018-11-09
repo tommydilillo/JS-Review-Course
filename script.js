70b7e6e8653e935d6ba8c5d7c438880d28069f66
Tommy DiLillo <tommydilillo@gmail.com>
2018-11-08 22:24:51 -0500

    2nd challenge

--- script.js
+++ script.js
@@ -117,21 +117,172 @@ console.log(x);
  * CODING CHALLENGE 1 
  */
 
-var markMass, markHeight, johnMass, johnHeight;
-markMass = 80; //kg
-markHeight = 2; // meters
-johnMass = 100;
-johnHeight = 1.5;
+// var markMass, markHeight, johnMass, johnHeight;
+// markMass = 80; //kg
+// markHeight = 2; // meters
+// johnMass = 100;
+// johnHeight = 1.5;
 
-markBMI = markMass / (markHeight * markHeight);
-console.log('Mark BMI:', markBMI);
-johnBMI = johnMass / (johnHeight * johnHeight);
-console.log('John BMI:', johnBMI);
+// markBMI = markMass / (markHeight * markHeight);
+// console.log('Mark BMI:', markBMI);
+// johnBMI = johnMass / (johnHeight * johnHeight);
+// console.log('John BMI:', johnBMI);
 
-markHigherBMI = markBMI > johnBMI;
-console.log(markHigherBMI);
-
-
+// markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
 
 
+/************
+ * If / Else Statements 
+ */
 
+
+// var firstName = 'John';
+// var civilStatus = 'single';
+
+// if (civilStatus === 'married') {
+//     console.log(firstName + ' is married!')
+// }
+// else {
+//     console.log(firstName + ' will hopefully marry soon :)')
+// }
+
+// var isMarried = true;
+// if (isMarried) {
+//     console.log(firstName + ' is married!')
+// }
+// else {
+//     console.log(firstName + ' will hopefully marry soon :)')
+// }
+
+// var markMass, markHeight, johnMass, johnHeight;
+// markMass = 80; //kg
+// markHeight = 2; // meters
+
+// johnMass = 100;
+// johnHeight = 1.5;
+
+// markBMI = markMass / (markHeight * markHeight);
+// johnBMI = johnMass / (johnHeight * johnHeight);
+
+// if (markBMI > johnBMI) {
+//     console.log('Mark\'s BMI is higher than John\'s.');
+// } else {
+//     console.log('John\'s BMI is higher than Mark\'s.');
+// }
+
+
+/************
+ * Boolean Logic
+ */
+
+// var firstName = 'John';
+// var age = 20;
+
+// if (age < 13) {
+//     console.log(firstName + ' is a boy');
+
+// } else if (13 <= age && age < 20) {
+//     console.log(firstName + ' is a teenager');
+// } else if (20 <= age && age < 30) {
+//     console.log(firstName + ' is a young man');
+// } else {
+//     console.log(firstName + ' is a man');
+// }
+
+/************
+ * The Ternary Operator and Switch Statements 
+ */
+
+// var firstName = 'John';
+// var age = 20;
+
+// // Ternary Operator
+// age >= 18 ? console.log(firstName + ' drinks beer.')
+//     : console.log(firstName + ' drinks juice.');
+
+// var drink = age >= 18 ? 'beer' : 'juice';
+// console.log(drink);
+
+// /*if (age >= 18) {
+//     var drink = 'beer';
+// } else {
+//     var drink = 'juice';
+// }*/
+
+
+// // Switch Statement
+
+// var job = 'designer';
+// switch (job) {
+//     case 'teacher':
+//     case 'instructor':
+//         console.log(firstName + ' teaches kids how to code.');
+//         break;
+//     case 'driver':
+//         console.log(firstName + ' drives an uber in Lisbon');
+//         break;
+//     case 'designer':
+//         console.log(firstName + ' designs beautiful websites');
+//         break;
+//     default:
+//         console.log(firstName + ' does something else');
+// }
+
+
+// switch (true) {
+//     case age < 13:
+//         console.log(firstName + ' is a boy');
+//         break;
+//     case age >= 13 && age < 20:
+//         console.log(firstName + ' is a teenager');
+//         break;
+//     case (age >= 20 && age < 30):
+//         console.log(firstName + ' is a young man');
+//         break;
+//     default:
+//         console.log(firstName + ' is a man');
+// }
+
+/************
+ * Truthy and Falsy values and equality operators
+ */
+
+// falsy values: undefined, null, 0, '', NaN 
+// truthy values: NOT falsy values
+
+/*var height;
+height = 23;
+
+if (height || height === 0) {
+    console.log('Variable is defined');
+} else {
+    console.log('Variable has NOT been defined');
+}
+
+// Equality operators
+if (height == '23') {
+    console.log('The == operator does type coercion!')
+}
+
+*/
+
+/************
+ * Coding Challenge 2
+ */
+
+var johnAvg = (89 + 120 + 103) / 3;
+var mikeAvg = (116 + 94 + 123) / 3;
+var maryAvg = (97 + 134 + 105) / 3;
+console.log(johnAvg, mikeAvg, maryAvg);
+
+
+if (johnAvg > mikeAvg && johnAvg > maryAvg) {
+    console.log('John Wins with ' + johnAvg + ' points');
+} else if (mikeAvg > johnAvg && mikeAvg > maryAvg) {
+    console.log('Mike Wins with ' + mikeAvg + ' points');
+} else if (maryAvg > johnAvg && maryAvg > mikeAvg) {
+    console.log('Mary Wins with ' + maryAvg + ' points');
+} else {
+    console.log('It\'s a tie!');
+}
